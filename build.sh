9f04d8363f1d50cd9b50ab59cb96880983cdbbaa
Jay W <40270505+JayDoubleu@users.noreply.github.com>
2021-12-20 03:28:01 +0000

    Add workflow parameters

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ github_username=$(echo $GITHUB_ACTOR|  tr '[:upper:]' '[:lower:]')
 
 dnf -y install dnf-plugins-core
 dnf -y copr enable @CoreOS/continuous
-dnf -y install git docker skopeo rpm-ostree-2021.14.79.ga30dec3f-1.fc35.x86_64
+dnf -y install git docker skopeo $RPM_OSTREE_VERSION
 
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 
@@ -14,15 +14,15 @@ rpm-ostree --version
 
 pushd /tmp/container
 
-git clone -b f35 https://pagure.io/workstation-ostree-config
+git clone -b $SOURCE_REPOSITORY_BRANCH $SOURCE_REPOSITORY source_repository
 
-pushd workstation-ostree-config
+pushd source_repository
 
 mkdir -p repo cache && ostree --repo=repo init --mode=archive
-rpm-ostree compose tree --repo=repo --cachedir=cache fedora-silverblue.yaml
+rpm-ostree compose tree --repo=repo --cachedir=cache $SOURCE_REPOSITORY_MANIFEST
 ostree --verbose summary --repo=repo --update
 
 rpm-ostree ex-container encapsulate \
-	--repo=repo \
-	fedora/35/x86_64/silverblue \
-	docker://ghcr.io/$github_username/fedora-silverblue:35
+        --repo=repo \
+        $BUILD_DESTINATION_OSTREE_REFSPEC \
+        docker://ghcr.io/$github_username/$BUILD_DESTINATION_IMAGE_LABEL
